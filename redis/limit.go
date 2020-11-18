3268b066ebf3faa578b3c4a8142da4aa1e08b97f
caigaoxing <caigaoxing@douyu.tv>
2020-11-18 21:20:00 +0800

    add redis limit

---
+++ redis/limit.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"sync"
+	"time"
+)
+
+func limit() {
+
+	var luatxt = `
+local ratelimit_info = redis.pcall('HMGET',KEYS[1],'last_time','current_token')
+
+local last_time = ratelimit_info[1]
+
+local current_token = tonumber(ratelimit_info[2])
+
+local max_token = tonumber(ARGV[1])
+
+local token_rate = tonumber(ARGV[2])
+
+local current_time = tonumber(ARGV[3])
+
+local reverse_time = 1000/token_rate
+
+local result = 0
+
+if current_token == nil then
+
+  current_token = max_token
+
+  last_time = current_time
+
+else
+
+  local past_time = current_time-last_time
+  local reverse_token = math.floor(past_time/reverse_time)
+
+  if(reverse_token == 0 and current_token==0 ) then
+	return result
+  end 
+  current_token = current_token+reverse_token
+
+  last_time = reverse_time*reverse_token+last_time
+
+  if current_token>max_token then
+
+    current_token = max_token
+
+  end
+
+end
+
+
+if(current_token>0) then
+
+  result = 1
+
+  current_token = current_token-1
+
+end 
+
+redis.call('HMSET',KEYS[1],'last_time',last_time,'current_token',current_token)
+
+redis.call('pexpire',KEYS[1],math.ceil(reverse_time*(max_token-current_token)+(current_time-last_time)))
+
+return result`
+
+	var wait sync.WaitGroup
+	p := dial()
+	for i := 0; i < 8; i++ {
+		wait.Add(1)
+		go func() {
+			defer wait.Done()
+			fmt.Println(redis.Int64(p.Get().Do("eval", luatxt, 1, "limt_test", 5, 5, time.Now().UnixNano()/1000000)))
+		}()
+	}
+	wait.Wait()
+	fmt.Println(redis.StringMap(p.Get().Do("hgetall", "limt_test")))
+
+}
---
+++ redis/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"sync"
+	"time"
+)
+
+// dial dials connections then returns pool.
+func dial() *redis.Pool {
+	dialOptions := []redis.DialOption{
+		redis.DialConnectTimeout(time.Second),
+		redis.DialReadTimeout(time.Second),
+		redis.DialWriteTimeout(time.Second),
+	}
+	return &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", "127.0.0.1:6379", dialOptions...)
+			if err != nil {
+				return nil, err
+			}
+
+			return c, nil
+		},
+		MaxIdle:     1000,
+		MaxActive:   1000,
+		IdleTimeout: time.Second * 100,
+	}
+}
+
+func lua() {
+	p := dial()
+	// --------
+	fmt.Println(redis.String(p.Get().Do("get", "name")))
+
+	p.Get().Do("set", "name", "happy")
+	fmt.Println(redis.String(p.Get().Do("get", "name")))
+
+	// --------
+	var luatxt = `return redis.call('set',KEYS[1],'happylua')`
+	p.Get().Do("eval", luatxt, 1, "name")
+	fmt.Println(redis.String(p.Get().Do("get", "name")))
+}
+func main() {
+	//lua()
+	limit()
+}
+
