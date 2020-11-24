1b1494f4f0a21624863bdcf72bfc3420e1c3a66e
caigaoxing <caigaoxing@douyu.tv>
2020-11-24 14:49:59 +0800

    add test

--- validator/pkg.go
+++ validator/pkg.go
@@ -107,22 +107,4 @@ func Struct(data interface{}) error {
 	return va.Struct(data)
 }
 
-var regs = map[string]*Register{"key": &Register{Func: func(l validator.FieldLevel) bool {
-	return l.Field().String() == "1@163.com"
-}, Msg: "{0}illeagl"}}
 
-func main() {
-	Init("zh", regs)
-	type User struct {
-		Name  string `validate:"required" label:"姓名"`
-		Age   uint8  `validate:"gte=0,lte=130"`
-		Email string `validate:"required,email,key" label:"邮箱"`
-	}
-	var u User
-	u.Age = 190
-	u.Email = "2@163.com"
-	if err := Struct(u); err != nil {
-		fmt.Println(GetMsg(err))
-	}
-
-}
