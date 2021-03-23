ba5535caa3dfa9f3d19089a39825a240948381ad
caigaoxing <caigaoxing@douyu.tv>
2021-03-23 15:32:43 +0800

    add grpc + etcd

--- grpc/grpc-etcd/client.go
+++ grpc/grpc-etcd/client.go
@@ -1,33 +1,38 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/coreos/etcd/clientv3"
-	"github.com/coreos/etcd/clientv3/naming"
 	"github.com/ukinhappy/go-example/grpc/pkg/proto"
+	"time"
+
+	"github.com/coreos/etcd/clientv3/naming"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"log"
+	"strconv"
 )
 
 func main() {
-	cli, cerr := clientv3.NewFromURL("127.0.0.1:2379")
+	cli, cerr := clientv3.NewFromURL("http://127.0.0.1:2379")
 	if cerr != nil {
-		log.Fatal(cerr)
-	}
-	r := &naming.GRPCResolver{Client: cli}
-	b := grpc.RoundRobin(r)
-	conn, gerr := grpc.Dial("/grpc-etcd/server/127.0.0.1:18888", grpc.WithBalancer(b))
-
-	if gerr != nil {
-		log.Fatal(gerr)
+		panic(cerr)
 	}
 
-	client := proto.NewHelloWorldClient(conn)
-	resp, err := client.SayHelloWorld(context.TODO(), &proto.HelloWorldRequest{Referer: "happy"})
+	b := grpc.RoundRobin(&naming.GRPCResolver{Client: cli})
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	conn, err := grpc.DialContext(ctx, "/hello_service", grpc.WithInsecure(), grpc.WithBalancer(b))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	fmt.Println(resp.Message)
+	ticker := time.NewTicker(1 * time.Second)
+	for t := range ticker.C {
+		fmt.Println("client.....")
+		client := proto.NewHelloWorldClient(conn)
+		resp, err := client.SayHelloWorld(context.Background(), &proto.HelloWorldRequest{Referer: "world " + strconv.Itoa(t.Second())})
+		if err == nil {
+			fmt.Printf("%v: Reply is %s\n", t, resp.Message)
+		}
+		fmt.Println(err)
+	}
 }
--- grpc/grpc-etcd/server.go
+++ grpc/grpc-etcd/server.go
@@ -1,33 +1,100 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
 	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/etcdserver/api/v3rpc/rpctypes"
 	"github.com/ukinhappy/go-example/grpc/pkg/proto"
-	"google.golang.org/grpc"
+	"google.golang.org/grpc/naming"
 	"log"
 	"net"
 	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
-type HelloWorldService struct{}
+var (
+	serv = flag.String("service", "hello_service", "service name")
+	port = flag.Int("port", 50001, "listening port")
+)
 
-func (s *HelloWorldService) SayHelloWorld(ctx context.Context, r *proto.HelloWorldRequest) (*proto.HelloWorldResponse, error) {
-	return &proto.HelloWorldResponse{Message: r.GetReferer() + " hello"}, nil
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHelloWorld(ctx context.Context, in *proto.HelloWorldRequest) (*proto.HelloWorldResponse, error) {
+	fmt.Printf("%v: Receive is %s\n", time.Now(), in.Referer)
+	return &proto.HelloWorldResponse{Message: "Hello " + in.Referer}, nil
 }
+
 func main() {
-	listen, err := net.Listen("tcp", "127.0.0.1:18888")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", *port))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+
+	err = register("hello_service", "127.0.0.1", *port, "http://127.0.0.1:2379", time.Second*10, 15)
+	if err != nil {
+		panic(err)
+	}
+
 	s := grpc.NewServer()
-	proto.RegisterHelloWorldServer(s, &HelloWorldService{})
+	proto.RegisterHelloWorldServer(s, &server{})
+	s.Serve(lis)
+}
+
+// register
+func register(name string, host string, port int, etcdaddr string, interval time.Duration, ttl int) error {
+	service := naming.Update{
+		Addr:     fmt.Sprintf("%s:%d", host, port),
+		Metadata: "...",
+	}
+	bts, err := json.Marshal(service)
+	if err != nil {
+		return err
+	}
+	serviceValue := string(bts)
+	serviceKey := fmt.Sprintf("/%s/%s", name, serviceValue)
+
+	client, err := clientv3.New(clientv3.Config{
+		Endpoints: []string{etcdaddr},
+	})
+	if err != nil {
+		return fmt.Errorf("grpclb: create etcd3 client failed: %v", err)
+	}
+
 	go func() {
-		cli, err := clientv3.New(clientv3.Config{Endpoints: []string{"127.0.0.1:2379"}, DialTimeout: time.Second * 5})
-		if err != nil {
-			panic(err)
+		// invoke self-register with ticker
+		ticker := time.NewTicker(interval)
+		for {
+			// minimum lease TTL is ttl-second
+			resp, _ := client.Grant(context.TODO(), int64(ttl))
+			// should get first, if not exist, set it
+			_, err := client.Get(context.Background(), serviceKey)
+			if err != nil {
+				if err == rpctypes.ErrKeyNotFound {
+					if _, err := client.Put(context.TODO(), serviceKey, serviceValue, clientv3.WithLease(resp.ID)); err != nil {
+						log.Printf("grpclb: set service '%s' with ttl to etcd3 failed: %s", name, err.Error())
+					}
+				} else {
+					log.Printf("grpclb: service '%s' connect to etcd3 failed: %s", name, err.Error())
+				}
+			} else {
+				// refresh set to true for not notifying the watcher
+				if _, err := client.Put(context.Background(), serviceKey, serviceValue, clientv3.WithLease(resp.ID)); err != nil {
+					log.Printf("grpclb: refresh service '%s' with ttl to etcd3 failed: %s", name, err.Error())
+				}
+			}
+			select {
+			case <-ticker.C:
+			}
 		}
-		cli.Put(context.TODO(), "/grpc-etcd/server/127.0.0.1:18888", "127.0.0.1:18888")
 	}()
-	s.Serve(listen)
+
+	return nil
 }
